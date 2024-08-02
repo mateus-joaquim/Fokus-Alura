b8b972fc6cad061ac53f58dbb4c2a1ae91e8c94d
mateus-joaquim <m.joaquim.fs@gmail.com>
2024-08-02 18:30:28 -0300

    Mudança dinâmica do banner

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+//Elementos dinâmicos
 const html = document.querySelector('html');
+const displayTempo = document.querySelector('#timer');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title')
+
+//Botões
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const inicioBt = document.querySelector('.app__card-primary-button')
+
+//Tempo dos temporizadores
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png')
 })
 
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
 })
